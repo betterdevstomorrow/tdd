f8e74a5c4d9558951d381e61b2814a3a99fa8e4a
Hyunhoo-Kwon <kwongusgn@gmail.com>
2019-06-22 16:08:40 +0900

    Update SubDate.java

--- tdd-example/src/main/java/SubDate.java
+++ tdd-example/src/main/java/SubDate.java
@@ -1,13 +1,24 @@
 public class SubDate {
 
     public int getYearDay(int year) {
-        if (year==1 )
-            return 0;
-        else
-            return 365;
+        if(year<0)
+            throw new RuntimeException("Invalid year");
+
+        int result = 0;
+        for(int i=1; i<year; i++) {
+            if (isLeapYear(i))
+                result += 366;
+            else
+                result += 365;
+        }
+        return result;
     }
 
     public boolean isLeapYear(int year) {
+        if (year % 400 == 0)
+            return true;
+        if (year % 100 == 0)
+            return false;
         if (year % 4 == 0)
             return true;
         return false;
