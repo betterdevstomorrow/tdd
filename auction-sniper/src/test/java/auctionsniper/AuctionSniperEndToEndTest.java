e98ace8bfefdd80628b3712c7a464f0ac81f4d6c
Maisy <linari21@gmail.com>
2019-06-30 10:59:20 +0900

    add chapter03

---
+++ auction-sniper/src/test/java/auctionsniper/AuctionSniperEndToEndTest.java
@@ -0,0 +1,32 @@
+package auctionsniper;
+
+import org.jivesoftware.smack.XMPPException;
+import org.junit.After;
+import org.junit.Test;
+
+
+public class AuctionSniperEndToEndTest {
+	// 가짜 경매서비스(stub)
+	private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+	private final ApplicationRunner application = new ApplicationRunner();
+
+	@Test
+	public void sniperJoinsAuctionUntilAuctionCloses() throws XMPPException, InterruptedException {
+		auction.startSellingItem();
+		application.startBiddingIn(auction);
+		auction.hasReceivedJoinRequestFromSniper();
+		auction.announceClosed();
+		application.showsSniperHasLostAuction();
+	}
+
+	@After
+	public void stopAuction() {
+		auction.stop();
+	}
+
+	@After
+	public void stopApplication() {
+		application.stop();
+	}
+
+}
